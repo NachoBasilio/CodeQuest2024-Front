5d183ced2323a75bd63ecb850f7cbede4eecbbd4
Jakiro12 <eclipsisnocte@gmail.com>
2024-03-13 12:08:49 -0300

    feat:adding mockup design

--- src/app/page.js
+++ src/app/page.js
@@ -2,94 +2,34 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const titleWeb="{Dev/Talles} Sorteos"
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <h3 className={styles.title_web_lottery}>{titleWeb}</h3>
+      <article className={styles.reward_container}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+        titulo del Sorteos
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
         <Image
-          className={styles.logo}
           src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          alt="imagen del premio actual"
+          width={200}
+          height={200}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <p>
+        foto del premio
+        contador u rango de fecha
+        </p>
+      </article>
+      <section className={styles.get_in_lottery}>
+        <p>
+          Unete a nuestro canal de discord y participa con tu ID de discord
           </p>
-        </a>
-      </div>
+        <form>
+          <input className={styles.get_id_discord} type="text"/>
+          <button className={styles.btn_verify_id_dscord} type="submit">Participar</button>
+        </form>
+      </section>
     </main>
   );
 }
